b5492dbb1a23cbbc0cae08e1339898603d42d328
Andriy Zakhozhuy <andriy.zakhozhuy@gmail.com>
2026-07-17 12:34:46 +0300

    Install Meta Pixel and TikTok Pixel, track vote_click in both

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,8 @@ export const metadata: Metadata = {
 };
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+const META_PIXEL_ID = '1553650003125054';
+const TIKTOK_PIXEL_ID = 'D9CV9U3C77U2EG6DM390';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -34,22 +35,40 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </Script>
           </>
         )}
-        {META_PIXEL_ID && (
-          <Script id="meta-pixel-init" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${META_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-        )}
+        <Script id="meta-pixel-init" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height={1}
+            width={1}
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        <Script id="tiktok-pixel-init" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+              ttq.load('${TIKTOK_PIXEL_ID}');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
       </body>
     </html>
   );
--- lib/analytics.test.ts
+++ lib/analytics.test.ts
@@ -10,18 +10,20 @@ describe('trackVoteEvent', () => {
     expect(() => trackVoteEvent()).not.toThrow();
   });
 
-  it('calls gtag and fbq when present on window', () => {
+  it('calls gtag, fbq, and ttq when present on window', () => {
     const gtag = vi.fn();
     const fbq = vi.fn();
-    vi.stubGlobal('window', { gtag, fbq });
+    const ttq = { track: vi.fn() };
+    vi.stubGlobal('window', { gtag, fbq, ttq });
 
     trackVoteEvent();
 
     expect(gtag).toHaveBeenCalledWith('event', 'vote_click');
     expect(fbq).toHaveBeenCalledWith('track', 'Lead');
+    expect(ttq.track).toHaveBeenCalledWith('ClickButton');
   });
 
-  it('does not throw when gtag/fbq are not defined', () => {
+  it('does not throw when gtag/fbq/ttq are not defined', () => {
     vi.stubGlobal('window', {});
     expect(() => trackVoteEvent()).not.toThrow();
   });
